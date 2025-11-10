e831c380ec11c42f0a3c91d09b4f9c9d8276487a
Opahgrammer <gamingfajar438@gmail.com>
2025-11-10 10:18:27 +0700

    dagshub

---
+++ dagshub_init.py
@@ -0,0 +1,7 @@
+import dagshub
+
+dagshub.init(
+    repo_owner='Opahgrammer',   # username GitHub kamu
+    repo_name='Eksperimen_SML_Fajar',  # nama repo kamu di GitHub & DagsHub
+    mlflow=True                 # ini yang mengaktifkan MLflow tracking
+)
